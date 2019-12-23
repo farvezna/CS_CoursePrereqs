deff61b53a78f9647fc8e4381d57c8a451bdfe2f
Nabid Farvez <farvezna@gmail.com>
2019-12-23 17:38:51 -0500

    Added csv file parsing

---
+++ course_regex.py
@@ -0,0 +1,18 @@
+import re
+import csv
+from pprint import pprint
+
+prereq_dict = {}
+course_dict = {}
+
+with open('cs_prereqs2018_2.csv') as csv_file:
+    reader = csv.DictReader(csv_file, delimiter=",")
+    for row in reader:
+        course_id = row['SUBJECT'] + row['NUMB']
+        prereq_dict[course_id] = row['PRE-REQ COURSES']
+        course_dict[course_id] = row['CRSE TITLE']
+
+course = input("What course are you trying to take? : ").upper().replace(" ", "")
+#course = "".join([char if char != " " else "" for char in course])
+print("Requirements for " + course_dict[course] + ": ")
+print(prereq_dict[course])
