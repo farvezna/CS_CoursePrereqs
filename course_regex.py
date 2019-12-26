c8b0ab388b48e5d483b0013ac9bc2512836187f0
Nabid Farvez <farvezna@gmail.com>
2019-12-26 17:09:20 -0500

    Fixed regex to handle chunking of OR courses

--- course_regex.py
+++ course_regex.py
@@ -9,9 +9,10 @@ with open('cs_prereqs2018_2.csv') as csv_file:
     reader = csv.DictReader(csv_file, delimiter=",")
     for row in reader:
         course_id = row['SUBJECT'] + row['NUMB']
-        prereqs = re.findall('[A-Z]+\s[0-9]{4}', row['PRE-REQ COURSES'])
-        print(prereqs)
-        prereq_dict[course_id] = prereqs if prereqs.__len__() > 0 else "None"
+        prereqs = re.findall("(?:[A-Za-z/]*\s*[0-9X]{4}(?:\sor\s)*)+", row['PRE-REQ COURSES'])
+        print(prereqs, end="")
+        print(" : "  + row['NUMB'])
+        prereq_dict[course_id] = prereqs
         course_dict[course_id] = row['CRSE TITLE']
 
 #course = input("What course are you trying to take? : ").upper().replace(" ", "")
@@ -21,4 +22,4 @@ with open('cs_prereqs2018_2.csv') as csv_file:
 
 #TODO: regex of prereq courses, List for OR's, tuple in list for AND's
 #pprint(prereq_dict)
-pprint(prereq_dict)
+#pprint(prereq_dict)
