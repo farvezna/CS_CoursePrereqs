c87d7748aba9efb395f7044e045f79ad99dbfb14
Nabid Farvez <farvezna@gmail.com>
2019-12-26 15:44:50 -0500

    Added basic regex for parsing course ID's to prereq dict

--- course_regex.py
+++ course_regex.py
@@ -9,12 +9,16 @@ with open('cs_prereqs2018_2.csv') as csv_file:
     reader = csv.DictReader(csv_file, delimiter=",")
     for row in reader:
         course_id = row['SUBJECT'] + row['NUMB']
-        prereq_dict[course_id] = row['PRE-REQ COURSES']
+        prereqs = re.findall('[A-Z]+\s[0-9]{4}', row['PRE-REQ COURSES'])
+        print(prereqs)
+        prereq_dict[course_id] = prereqs if prereqs.__len__() > 0 else "None"
         course_dict[course_id] = row['CRSE TITLE']
 
-course = input("What course are you trying to take? : ").upper().replace(" ", "")
+#course = input("What course are you trying to take? : ").upper().replace(" ", "")
 #course = "".join([char if char != " " else "" for char in course])
-print("Requirements for " + course_dict[course] + ": ")
-print(prereq_dict[course])
+#print("Requirements for " + course_dict[course] + ": ")
+#print(prereq_dict[course])
 
 #TODO: regex of prereq courses, List for OR's, tuple in list for AND's
+#pprint(prereq_dict)
+pprint(prereq_dict)
